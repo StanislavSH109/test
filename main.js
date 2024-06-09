c6b6330b842fe06eaa332b29a9d07eb8dcc3dbe4
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-06-09 13:58:33 +0300

    tags and js tasks

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-let user_name;
-let surname;
-let age;
+function squareSum(numbers){
+    let nums_len = numbers.length;
+    let result = 0;
+    
+    for (let i = 0; i < nums_len; i++) {
+      result += numbers[i] ** 2;
+      console.log(result);  
+    }
+    
+    return result;
+  }
 
-user_name = 'Neo', surname = 'mr.Andersen', age = '40';
 
-console.log(user_name, surname, age);
+squareSum(nums = [1, 2, 2]);
+ 
+
+
+// let user_name;
+// let surname;
+// let age;
+
+// user_name = 'Neo', surname = 'mr.Andersen', age = '40';
+
+// console.log(user_name, surname, age);
+
+
